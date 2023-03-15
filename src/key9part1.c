f710789b1a3ebdcf1a7e0feaad8672c680104502
Jane Charlott <janechar@ev-c3.kzn.21-school.ru>
2023-03-15 19:39:19 +0300

    2

--- src/key9part1.c
+++ src/key9part1.c
@@ -1,48 +1,35 @@
-/*------------------------------------
-        Здравствуй, человек!
-        Чтобы получить ключ
-        поработай с комментариями.
--------------------------------------*/
-
 #include <stdio.h>
 #define NMAX 10
 
-int input(int *buffer, int *length);
-void output(int *buffer, int length);
-int sum_numbers(int *buffer, int length);
-int find_numbers(int *buffer, int length, int number, int *numbers);
+int input(int *a, int *n);
+void output(int *a, int n);
+int sum_numbers(int *a, int n);
+int find_numbers(int *a, int n, int number, int *numbers);
 
-/*------------------------------------
-        Функция получает массив данных
-        через stdin.
-        Выдает в stdout особую сумму
-        и сформированный массив
-        специальных элементов
-        (выбранных с помощью найденной суммы):
-        это и будет частью ключа
--------------------------------------*/
 int main() {
-    int data[NMAX], y;
-    int x = input(data, &y);
+    int n;
+    int data[NMAX];
+    int x = input(data, &n);
     if (x == 1) {
         printf("n/a\n");
     } else {
-        // output (data, y);
-        int z = sum_numbers(data, y);
-        printf("%d", z);
-        // find_numbers(data, int length, int number, int *numbers)
+        int z = sum_numbers(data, n);
+        printf("%d\n", z);
+        int numbers[NMAX];
+        int c = find_numbers(data, n, z, numbers);
+        output(numbers, c);
     }
     return 0;
 }
 
-int input(int *buffer, int *length) {
+int input(int *a, int *n) {
     int y, x;
     int z = 0;
-    x = scanf("%d.%d", length, &y);
-    if (x != 1 || *length <= 0 || *length > NMAX) {
+    x = scanf("%d.%d", n, &y);
+    if (x != 1 || *n <= 0 || *n > NMAX) {
         z = 1;
     } else {
-        for (int *p = buffer; p - buffer < NMAX; p++) {
+        for (int *p = a; p - a < NMAX; p++) {
             x = scanf("%d.%d", p, &y);
             if (x != 1) {
                 z = 1;
@@ -52,38 +39,32 @@ int input(int *buffer, int *length) {
     return z;
 }
 
-void output(int *buffer, int length) {
-    for (int *p = buffer; p - buffer < NMAX - 1; p++) {
+void output(int *a, int n) {
+    for (int *p = a; p - a < n - 1; p++) {
         printf("%d ", *p);
     }
-    printf("%d\n", *(buffer + length - 1));
+    printf("%d\n", *(a + n - 1));
 }
 
-/*------------------------------------
-        Функция должна находить
-        сумму четных элементов
-        с 0-й позиции.
--------------------------------------*/
-int sum_numbers(int *buffer, int length) {
+int sum_numbers(int *a, int n) {
     int sum = 0;
 
-    for (int i = 1; i < length; i++) {
-        if (i % 2 == 0) {
-            sum = sum + *buffer;
+    for (int i = 0; i < n; i++) {
+        if (a[i] % 2 == 0) {
+            sum = sum + a[i];
         }
     }
 
     return sum;
 }
 
-/*------------------------------------
-        Функция должна находить
-        все элементы, на которые нацело
-        делится переданное число и
-        записывает их в выходной массив.
--------------------------------------*/
-// int find_numbers(int *buffer, int length, int number, int *numbers)
-//{
-
-// return 0;
-// }
+int find_numbers(int *a, int n, int number, int *numbers) {
+    int f = 0;
+    for (int *p = a; p - a < n; p++) {
+        if (*p != 0 && number % *p == 0) {
+            numbers[f] = *p;
+            f = f + 1;
+        }
+    }
+    return f;
+}
